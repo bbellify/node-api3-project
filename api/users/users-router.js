0f2d10deb54892273253bd3917913fb7d07b21ac
Brian Bellinger <brianpatrickbellinger@gmail.com>
2021-12-01 13:24:27 -0800

    initial commit - server listening, router wired

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -5,40 +5,48 @@ const express = require('express');
 
 const router = express.Router();
 
+// RETURN AN ARRAY WITH ALL THE USERS
 router.get('/', (req, res) => {
-  // RETURN AN ARRAY WITH ALL THE USERS
+
 });
 
+// RETURN THE USER OBJECT
+// this needs a middleware to verify user id
 router.get('/:id', (req, res) => {
-  // RETURN THE USER OBJECT
-  // this needs a middleware to verify user id
+
 });
 
+// RETURN THE NEWLY CREATED USER OBJECT
+// this needs a middleware to check that the request body is valid
 router.post('/', (req, res) => {
-  // RETURN THE NEWLY CREATED USER OBJECT
-  // this needs a middleware to check that the request body is valid
+
 });
 
+// RETURN THE FRESHLY UPDATED USER OBJECT
+// this needs a middleware to verify user id
+// and another middleware to check that the request body is valid
 router.put('/:id', (req, res) => {
-  // RETURN THE FRESHLY UPDATED USER OBJECT
-  // this needs a middleware to verify user id
-  // and another middleware to check that the request body is valid
+
 });
 
+// RETURN THE FRESHLY DELETED USER OBJECT
+// this needs a middleware to verify user id
 router.delete('/:id', (req, res) => {
-  // RETURN THE FRESHLY DELETED USER OBJECT
-  // this needs a middleware to verify user id
+
 });
 
+// RETURN THE ARRAY OF USER POSTS
+// this needs a middleware to verify user id
 router.get('/:id/posts', (req, res) => {
-  // RETURN THE ARRAY OF USER POSTS
-  // this needs a middleware to verify user id
+
 });
 
+// RETURN THE NEWLY CREATED USER POST
+// this needs a middleware to verify user id
+// and another middleware to check that the request body is valid
 router.post('/:id/posts', (req, res) => {
-  // RETURN THE NEWLY CREATED USER POST
-  // this needs a middleware to verify user id
-  // and another middleware to check that the request body is valid
+  
 });
 
 // do not forget to export the router
+module.exports = router
